2ee53a99b9625a6cb501f83685527045d56e9a1b
devin <906348933@qq.com>
2019-01-10 14:40:50 +0800

    创建项目

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component,createElement } from 'react';
 import './App.css';
 
+class ActionComponet extends Component {
+  render(){
+      return (<BaseModule {...this.props}/>)
+  }
+}
 class App extends Component {
+  constructor(props){
+      super(props);
+      this.state={
+          originalData: {
+              tag: 'div',
+              props: {style:'background:red'},
+              events: null,
+              children: 'hello'
+          }
+      };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Tool"></div>
+        <div id="WorkSpace">
+            <ActionComponet {...this.state.originalData}/>
+        </div>
       </div>
     );
+  }
+}
+class BaseModule extends Component {
+  render() {
+     const _props=Object.assign({},this.props,this.props.events)
+     const _children=typeof this.props.children==='object'?this.props.children.map(val=><BaseModule {...val}/>):this.props.children
+     return createElement(this.props.tag,_props,_children)
   }
 }
 
